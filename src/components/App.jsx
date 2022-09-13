a73f607598ba2e46240b76224b7c48fa4584b55f
dimaniagara <ds.sugak@gmail.com>
2022-09-13 00:44:43 -0400

    Updates_2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,75 @@
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+import { useState, useEffect } from 'react';
+import { Contacts } from './Contacts/Contacts';
+import { Phonebook } from './Phonebook/Phonebook';
+import { Filter } from './Filter/Filter';
+import { Box } from './Box';
+import { GlobalStyle } from './GlobalStyles';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    if (
+      localStorage.getItem('contacts') !== null &&
+      localStorage.getItem('contacts') !== '[]'
+    ) {
+      const localContacts = localStorage.getItem('contacts');
+      const parsedContacts = JSON.parse(localContacts);
+      setContacts(parsedContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const updateContacts = (id, name, number) => {
+    setContacts(prevState => [
+      ...prevState,
+      { id: id, name: name, number: number },
+    ]);
+  };
+
+  const updateFilter = filter => {
+    setFilter(filter);
+  };
+
+  const deleteContact = id => {
+    const newContacts = contacts.filter(element => element.id !== id);
+    setContacts(newContacts);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Box p="20px">
+      <h2>Phonebook</h2>
+      <Phonebook contacts={contacts} updateContacts={updateContacts} />
+
+      <h2>Contacts</h2>
+      <Filter filter={filter} updateFilter={updateFilter} />
+      <Contacts
+        contacts={contacts.filter(contact =>
+          contact.name.toLowerCase().includes(filter)
+        )}
+        deleteContact={deleteContact}
+      />
+      <GlobalStyle />
+    </Box>
   );
 };
---
+++ src/components/ContactsItem/ContactsItem.jsx
@@ -0,0 +1,20 @@
+import { ContactsItemWrapper } from './ContactsItem.styled';
+import PropTypes from 'prop-types';
+
+export const ContactsItem = ({ name, number, id, deleteContact }) => {
+  return (
+    <ContactsItemWrapper>
+      <p>
+        {name}: {number}
+      </p>
+      <button onClick={() => deleteContact(id)}>Delete</button>
+    </ContactsItemWrapper>
+  );
+};
+
+ContactsItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  deleteContact: PropTypes.func.isRequired,
+};
